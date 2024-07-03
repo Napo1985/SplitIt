4bcae1619f114154ce8d9e882e0650faea19a803
Napo1985 <harnoy85@gmail.com>
2024-07-03 22:58:20 +0300

    Dev (#1)
    * dev
    
    * commit progress not working
    
    * working api calls - need to fix the main with startup
    need to use in memory db
    
    * commit changes in the imdb class and db related
    
    * imdb working with data
    
    * duplication and request to dto to model
    
    * error handling
    
    * add unit tests
    
    * swagger content
    
    * add exception handling (OutOfRange)

--- Program.cs
+++ Program.cs
@@ -1,26 +1,63 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+using Splitit.App.Middlewares;
+using Splitit.Infra.Providers;
+using Splitit.Infra.Repositories;
+using Splitit.Splitit.Repositories;
+using Splitit.Splitit.Services;
+using Swashbuckle.AspNetCore.Filters;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        builder.Configuration.AddEnvironmentVariables();
+
+        ConfigureServices(builder);
+
+        var app = builder.Build();
+        Configure(app);
+
+        app.Run();
+    }
+
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+
+        services.AddControllers();
+        services.AddHttpClient();
+        services.AddSingleton<IActorRepository, InMemoryActorRepository>();
+
+        var configuration = builder.Configuration;
+        var actorProviderConfig = configuration.GetSection("ActorProviders");
+
+        //for providers we should use dynamic factory (at this scope will not be implemnted due to time limitation) 
+        services.AddTransient<IActorProvider, ImdbActorProvider>();
+        services.AddScoped<ActorService>();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Splitit API", Version = "Home Assignment" });
+            c.ExampleFilters();
+        });
+        services.AddSwaggerExamplesFromAssemblyOf<Program>();
+    }
+
+    private static void Configure(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
